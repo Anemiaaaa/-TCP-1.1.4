41021912e057f5f7094c976b38e68f8898e28b21
Anemia <amirimam2018@gmail.com>
2025-02-07 02:56:00 +0000

    Контейнеризация сервера

---
+++ task3.2.2.2/go.mod
@@ -0,0 +1,5 @@
+module studentgit.kata.academy/alikkamalrus_gmail.com/go-kata/course3/2.server/2.server_http/task3.2.2.2
+
+go 1.19
+
+require github.com/joho/godotenv v1.5.1
---
+++ task3.2.2.2/main.go
@@ -0,0 +1,24 @@
+// Пример кода для сервера на пакете net/http
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Сервер запущен на порту %s", port)
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
